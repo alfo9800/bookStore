68b0d760d1caa7a7e7d217c21c8afd9b503bfe25
User <User@DESKTOP-RFMB8MO>
2022-04-12 17:23:48 +0900

    lombok @setter custom

--- bookStore/src/main/java/com/bookStore/model/Criteria.java
+++ bookStore/src/main/java/com/bookStore/model/Criteria.java
@@ -1,5 +1,6 @@
 package com.bookStore.model;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,11 +11,31 @@ import lombok.ToString;
 
 public class Criteria {
 
-	/* 현재 페이지 */
+	/*
+	// 현재 페이지  
 	private int pageNum;
 	
-	/* 한 페이지 당 보여질 게시물 갯수 */
+	// 한 페이지 당 보여질 게시물 갯수
 	private int amount;
+	*/
+	
+	
+	/* 현재 페이지 */ //set할 때 특별히 커스텀을 해야하는 상황
+	@Setter(AccessLevel.PROTECTED)
+	private int pageNum;
+	public void setPageNum(int pageNum) {
+		this.skip = (pageNum-1)*this.amount;
+		this.skip = pageNum;
+	}
+	
+	/* 한 페이지 당 보여질 게시물 갯수 */ //set할 때 특별히 커스텀을 해야하는 상황
+	@Setter(AccessLevel.PROTECTED)
+	private int amount;
+	public void setAmount(int amount) {
+		this.skip = (this.pageNum-1)*amount;
+		this.amount = amount;
+	}
+	
 	
 	/* (Mysql) 스킵 할 게시물 수 -> (pageNum-1)*amount */
 	private int skip;
@@ -22,7 +43,7 @@ public class Criteria {
 	/* 기본 생성자 -> 기본 셋팅 : pageNum=1, amount=10 */ //파라미터 없이 Criteria클래스를 호출하였을 때
 	public Criteria() {
         this(1,10);
-        this.skip = 0; //(Mysql)
+        this.skip = 0; //(Mysql) 쿼리문의 limit에서 사용할 스킵 개수를 미리 java단계에서 처리 후 넘겨주기 위함
     }
 	
 	/* 생성자 -> 원하는 pageNum, 원하는 amount */ //파라미터와 함께 Criteria를 호출하게 되면 파라미터의 값이 각각 저장되도록 생성자 작성
